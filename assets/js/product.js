0623e708944ec5e11353dfdfb1b8f33c9baa3dff
Arsy Atturabi <cleovissv@gmail.com>
2025-08-19 16:42:03 +0700

    product.html

---
+++ assets/js/product.js
@@ -0,0 +1,48 @@
+const products = [
+  {
+    id: 1,
+    name: "Ajazz AJ159",
+    price: 49.19,
+    image: "assets/img/Ajazz_apex.png",
+    description: "Gaming Mouse 3-Modes PAW3950 APEX-AJ Flagship Sensor up to 42000 DPI with Magnetic Charging Dock.",
+    specs: [
+      "Connection : 2.4G Wireless, Type-C Wired & Bluetooth",
+      "Weight : About 56g",
+      "Size : 118.1*63.1*37.7mm",
+      "Sensor : PAW3950 APEX Ajazz Customized Flagship Sensor",
+      "Polling Rate : Wired 1K & Wireless 8K"
+    ]
+  },
+    {
+        id: 2,
+        name: "Logitech G Pro X 2 Superlight",
+        price: 200,
+        image: "assets/img/Superlight.png",
+        description: "A new weapon of choice for the world's top esports athletes.",
+        specs: [
+        "Tinggi: 125,0 mm",
+        "Lebar: 63,5 mm",
+        "Tebal: 40,0 mm",
+        "Berat: 60 g",
+        "Report rate USB: 1.000 Hz (1 ms)",
+        "Resolusi: 100 – 25.400 DPI"
+        ]
+    },
+    {
+        id: 3,
+        name: "Gamen Master",
+        price: 24.59,
+        image: "assets/img/GamenMaster.png",
+        description: "Affordable gaming laptop with AMD Ryzen 5, 8GB RAM, and NVIDIA GeForce GTX 1650.",
+        specs: [
+        "Polling Rate : 1000 Hz",
+        "Sensor : Pixart PAW 3395",
+        "Switch : Huano 80 Million",
+        "Sensor : Pixart PAW 3395",
+        "Switch : Huano 80 Million",
+        "DPI : Up to 26000 DPI (Adjustable with Software Driver)",
+        "Acceleration : 50 G (Max)",
+        "Sensor: HERO"
+        ]
+    },
+];
